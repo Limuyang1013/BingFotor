884468e73abe82259c41ef1fa3c727e5af58d655
Limuyang <lin794653318@gmail.com>
2016-11-17 16:46:35 +0800

    获取每日Bing图片作为壁纸

---
+++ app/src/main/java/com/wallpaper/bingfotor/activity/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.wallpaper.bingfotor.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Limuyang on 2016/11/17.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/wallpaper/bingfotor/constant/API.java
@@ -0,0 +1,8 @@
+package com.wallpaper.bingfotor.constant;
+
+/**
+ * Created by Limuyang on 2016/11/17.
+ */
+
+public class API {
+}
